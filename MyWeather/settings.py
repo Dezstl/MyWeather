fadaa4bbf7e63f88b4fbb45d0ca2f8ad853d33e8
Dezstl <dezmondm@slalom.com>
2014-04-29 14:41:50 -0500

    Db chagnes

--- MyWeather/settings.py
+++ MyWeather/settings.py
@@ -58,7 +58,7 @@ WSGI_APPLICATION = 'MyWeather.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'C:/Python34/MyWeather/weather.db',
     }
 }
 
