b26ca680ec387680a4eb6a353c4db87d0cf83b84
Dezstl <dezmondm@slalom.com>
2014-04-30 00:45:36 -0500

    model update

--- weather/models.py
+++ weather/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Locations(models.Model):
+	cityName = models.CharField(max_lenght=200)
+	cityID = models.IntegerField()
+	lon = models.CharField()
+	lat = modles.IntegerField()
