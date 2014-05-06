6337605c23ae86932b274c51fe4ad3bda93b1f28
Dezstl <dezmondm@slalom.com>
2014-05-06 00:07:27 -0500

    views

--- weather/urls.py
+++ weather/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import patterns, url
 from weather import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.home, name='home')
+    url(r'^$', views.apitest, name='apitest')
 )
--- weather/views.py
+++ weather/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from weather.models import Locations
 from django.template import Context, loader
 
+
 # Create your views here.
 
 def home(request):
@@ -10,3 +11,14 @@ def home(request):
 	t = loader.get_template('weather/index.html')
 	c = Context({'cities': cities,})
 	return HttpResponse(t.render(c))
+
+	
+def apitest(request):
+	try:
+		u = "http://api.openweathermap.org/data/2.5/weather?q={0}&units={1}"
+		r = requests.get(u.format(loc, units))
+		j = json.loads(r.text)
+	except:
+		sys.stderr.write("Couldn't load current conditions\n")
+	
+	return HttpResponse(j)
